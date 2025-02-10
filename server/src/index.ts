cc4741ca5ef79910734799c883bd535497ba606d
Jordy Van Den Kieboom <jordy.vandenkieboom@tactics.be>
2025-02-10 10:37:37 +0100

    vercel config

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import fastifyJWT from '@fastify/jwt';
 import fastifyCookie from '@fastify/cookie';
 import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
-import { prisma } from './lib/prisma';
+import { prisma } from './src/lib/prisma';
 
 dotenv.config();
 
